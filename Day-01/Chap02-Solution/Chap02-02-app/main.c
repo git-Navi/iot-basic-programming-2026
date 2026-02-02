8a04ec9586d1567d069f351ec7892fe016ea1bfb
git-Navi <122284636+git-Navi@users.noreply.github.com>
2026-02-02 17:30:02 +0900

    commit

---
+++ Day-01/Chap02-Solution/Chap02-02-app/main.c
@@ -0,0 +1,44 @@
+﻿// Chap02-02-app : 탈출문자(Escape Character), 변환문자(%) 학습
+#include <stdio.h>
+
+int main() {
+	printf("Hello, World!\n");	// 콘솔에 문자열을 format에 맞춰서 출력하는 함수
+
+	// '\n' escape character -> 한줄 내림 -> newline
+	printf("12345\n");
+	printf("67890\n");
+	printf("Wow!\n");
+
+	// printf 앞에서 Ctrl + C 하면 자동으로 한 줄로 복사가 되는 꿀팁이 있다.
+ 
+	// '\t' -> 콘솔에 탭 추가 -> tab
+	printf("My friend, Fuuka.\n");
+	printf("My\tfriend,\tFuuka.\n");
+
+	// '\b' -> 백스페이스 -> backspace
+	printf("Goot\bd Morning!\n");
+
+	// '\a' -> 알람 소리 -> alarm
+	printf("Bark!!\a\n");
+
+	// '\r' -> 글자 쓰는 맨 앞으로 이동 -> returncarrige
+	printf("Department\rLotte\n");
+
+	// 숫자를 콘솔에 출력하는 기능
+	
+	// '%d' -> deciaml, 정수를 문자로 출력
+	printf("%d\n", 10);
+	// '%nd' -> n의 자릿수만큼 늘려서 출력
+	printf("%10d\n", 10);
+	// '%f' -> float, 실수를 문자로 출력
+	printf("%f\n", 3.141592);
+	// '%lf' -> long float, 실수를 소수점 6자리까지 출력
+	printf("%lf\n", 3.14);
+	// '%.nf' -> 소수점 n자리까지 출력하고, 그 뒤는 반올림
+	printf("%.2f\n", 3.14159265727);
+
+	// 여러 개의 파라미터
+	printf("%5d x %5d = %5d", 6, 7, 6 * 7);
+
+	return 0;
+}
---
+++ Day-01/Chap02-Solution/Chap02-03-app/main.c
@@ -0,0 +1,36 @@
+﻿// 진법 표시
+#include <stdio.h>
+#include <limits.h>	// 각 타입별 최대값과 최소값을 저장하는 헤더 파일
+
+int main() {
+	// limits.h 사용해서 출력, 더 자세한 정보를 보고 싶으면 Ctrl + 클릭으로 참고
+	printf("int의 최대값 : %d\n", INT_MAX);
+	printf("int의 최소값 : %d\n", INT_MIN);
+
+	printf("%d\n", 12);		// 10진수
+	printf("%d\n", 014);	// 8진수, 12 (숫자 앞에 '0')
+	printf("%d\n", 0xC);	// 16진수, 12 (숫자 앞에 '0x')
+
+	printf("%d\n", 12);	// 10진수
+	printf("%o\n", 12);	// 12를 8진수로 출력
+	printf("%x\n", 12);	// 12를 16진수로 출력 (소문자)
+	printf("%X\n", 12);	// 12를 16진수로 출력 (대문자)
+
+	// 지수 표현
+	printf("%.1lf\n", 1e6);
+	printf("%.7lf\n", 3.14e-5);
+
+	// 문자, 문자열
+	// C에서는 문자 != 문자열
+	printf("%c\n", 'A');
+	printf("%s\n", "A");
+	printf("%d\n", 'A'); // 아스키코드로 A는 int로 65
+	printf("%c는 %s 입니다.\n", '1', "first");
+
+	// 문자는 무조건 변환문자 %c 사용
+	// 문자열은 무조건 변환문자 %s 사용
+	printf("%c는 %c 입니다.\n", '1', "first"); // c는 문자이기 때문에 first 작성X
+	printf("%s는 %s 입니다.\n", '1', "first"); // s는 문자열이기 때문에 1 작성X
+
+
+}
