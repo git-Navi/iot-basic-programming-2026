1550d14b1a8582a4a5ced90aa53a56881c44263e
git-Navi <122284636+git-Navi@users.noreply.github.com>
2026-02-13 09:36:36 +0900

    9일차 수정 commit

--- Day-09/Chap19-Solution/Chap19-03-app/average.c
+++ Day-09/Chap19-Solution/Chap19-03-app/average.c
@@ -1,7 +1,7 @@
 // 평균 계산 소스코드
 
-extern int count;
-extern int total;	// main.c에 있는 전역변수를 공유
+extern int count;	// main.c에 있는 count을 가져와서 공유!
+extern int total;	// input.c에 있는 total을 가져와서 공유!, main.c에 선언된 static total은 main.c 내에서만 공유됨.
 
 double average(void) {
 	return total / (double)count;
